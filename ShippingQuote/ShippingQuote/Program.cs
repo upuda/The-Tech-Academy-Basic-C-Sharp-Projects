7170b32dc85cebc6a725154b50793836030d7308
upuda <Upanya.Udayendran@my-aolcc.com>
2025-02-09 01:00:00 -0500

    Commit after completing the shipping quote assignment

---
+++ ShippingQuote/ShippingQuote/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ShippingQuote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            //To get the package weight
+            Console.WriteLine("Please enter the package weight: ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            //To get the package dimensions
+            Console.WriteLine("Please enter the package width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package height: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            
+            //To get the dimensions total
+            int total = width + height + length;
+            if(total > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return;
+            }
+
+            int productMultiple = width * height * length;
+            decimal quote = Math.Round((productMultiple * weight) / 100m, 2);
+
+            Console.WriteLine("Your estimated total for shipping this package is: "+quote);
+            Console.WriteLine("Thank you!");
+
+
+
+        }
+    }
+}
