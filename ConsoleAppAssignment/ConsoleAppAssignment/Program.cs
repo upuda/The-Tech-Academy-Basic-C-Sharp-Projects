dd3631f63cade9eb0869aef608e7eebf72b4d07d
upuda <Upanya.Udayendran@my-aolcc.com>
2025-02-09 01:16:24 -0500

    commit after completing Console app assignment

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int counter = 1;
+        //Using while loop
+        while (counter <= 5)
+        {
+            Console.WriteLine("Counter is: " + counter);
+            counter++;
+        }
+
+        int num = 10;
+        //Using do-while loop
+        do
+        {
+            Console.WriteLine("Number is: " + num);
+            num++;
+        } while (num < 15);
+        }
+    }
+
